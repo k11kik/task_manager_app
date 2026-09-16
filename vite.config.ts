7324caf73fb3ca62ff4fef6cfce994f84380f064
k11kik <143200648+k11kik@users.noreply.github.com>
2026-09-17 06:08:43 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
+  const basePath = process.env.VERCEL ? '/' : '/task_manager_app/';
   return {
-    base: '/task_manager_app/',
+    base: basePath,
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
